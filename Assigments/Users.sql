fa0a9516e4300e8ee0cbcd46407f22bd48e2f4b9
RomanceTheHeart <gerardpaul@outlook.com>
2022-05-22 11:25:36 -0600

    Mock Data for Assigment.

---
+++ Assigments/Users.sql
@@ -0,0 +1,7 @@
+insert into Users ();
+insert into (id, first_name,last_name,email,user_name) values(int, 'Zibadi','Mike','catchthezibabies@sumdum.com','Zibadi Mike');
+insert into (id, first_name,last_name,email,user_name) values(int, 'Jackson','Blackson','actionjackson@sumdum.com','Action Jackson');
+insert into (id, first_name,last_name,email,user_name) values (int, 'Amy', 'Anderson','flowersforamy@sumdum.com', 'Amy Flower');
+insert into (id, first_name,last_name,email,user_name) values (int, 'latifah', 'Muhammand','queenlatifah.yahoo.com', 'Queen Bee');
+
+
