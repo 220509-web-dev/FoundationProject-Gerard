ef19ff8d8c9d50679d61a29d1e0c7732250c0619
Gerard E. Paul <gerardpaul@outlook.com>
2022-05-29 17:34:05 -0500

    Scripted the database connection tool but I can't link to my VM.

--- Assigments/Week_3/Maven/CartoonApp/src/main/java/dev/gerard/app/ConnectionApp.java
+++ Assigments/Week_3/Maven/CartoonApp/src/main/java/dev/gerard/app/ConnectionApp.java
@@ -2,7 +2,7 @@ package dev.gerard.app;
 import dev.gerard.utils.ConnectionFactoryUtility;
 
 
-public class App {
+public class ConnectionApp {
 
     public static void main(String[] args) {
         System.out.println(ConnectionFactoryUtility.getConnection());
